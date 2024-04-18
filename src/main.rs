f1f3466e462e6adec759b53828d1904cc09ba28c
nourAhmed123 <96887154+nourAhmed123@users.noreply.github.com>
2024-04-18 19:44:52 +0200

    Structs

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@
 //mod vars;
 //mod types;
 //mod strings;
-//mod tuples;
+mod tuples;
 //mod arrays;
 //mod vectors;
 //mod conditionals;
 //mod loops;
 //mod functions;
 //mod pointer_ref;
-mod structs;
-mod ownership_example;
+//mod structs;
+//mod ownership_example;
 //mod enums;
 //mod cli;
 fn main() {
@@ -18,15 +18,15 @@ fn main() {
 //vars::run();
 //types::run();
 //strings::run();
-//tuples::run();
+tuples::run();
 //arrays::run();
 //vectors::run();
 //conditionals::run();
 //loops::run();
 //functions::run();
 //pointer_ref::run();
-structs::run();
-ownership_example::run();
+//structs::run();
+//ownership_example::run();
 //enums::run();
 //cli::run();
 }
--- src/tuples.rs
+++ src/tuples.rs
@@ -1,6 +1,11 @@
 //Tuples group together values of different types
 //Max 12 elements
+//Tuples doesnot have field names 
+//Tuple struct
+//accessing tuples with dot notation 
+struct Point (f64,f64,f64);
 pub fn run(){
+let point=Point(0.0,1.0,2.0);
 let person:(&str,&str,i8)=("Brad","Mass",37);
 println!("{} is from {} and is {}",person.0,person.1,person.2);
 
